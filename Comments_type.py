08efc3bab09d222ed478fb7d24eea6b673dea85e
Mayur Jiyani <mayurjiyani007@gmail.com>
2026-05-05 11:15:07 +0530

    Add files via upload

---
+++ Comments_type.py
@@ -0,0 +1,15 @@
+# Creating a Comment
+
+   #This is a comment
+print("Hello, World!") 
+
+
+# Multiline Comments
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
