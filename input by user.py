1f4f1dfce93c2f8b4d1b8f36b15241b6ee85dab0
Mayur Jiyani <mayurjiyani007@gmail.com>
2026-05-05 13:39:42 +0530

    Add files via upload

---
+++ input by user.py
@@ -0,0 +1,8 @@
+Name=input("Enter your product Name:")
+print("product Name", Name)
+
+price=int(input("Enter product price:"))
+print("product price", price)
+
+wight=float(input("Enter product wight:"))
+print("product wight", wight)
